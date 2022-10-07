a63d8ea13a4930b490a03eae444cfa17878e2eed
svetlana <nooch@yandex.ru>
2022-10-07 18:32:55 +0300

    программа котораявыводит случайное 3х значное чилсло и удаляетвтороу цифру этого числа

---
+++ 0002/Program.cs
@@ -0,0 +1,12 @@
+﻿//Задача 2: Напишите программу, которая выводит случайное трёхзначное число 
+//и удаляет вторую цифру этого числа. Не использовать строки для расчета.
+
+//456 -> 46
+//782 -> 72
+//918 -> 98
+
+int number = new Random().Next(100, 1000);
+Console.WriteLine(number);
+int number1 = number /100;
+int number3 = number % 10;
+Console.WriteLine($"{number1}{number3}");
